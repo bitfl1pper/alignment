750e883fb257a51a98bbef02fbc4d6eaffb40583
bitfl1pper <bitfl1pper@users.noreply.github.com>
2014-10-03 15:42:00 -0400

    Made the documentation more unreadable and tedious

--- src/alignment/global.clj
+++ src/alignment/global.clj
@@ -11,6 +11,20 @@
    Needleman and Wunsch [*] and as taught by the textbook: HANDBOOK OF
    COMPUTATIONAL MOLECULAR BIOLOGY [**]
 
+   This is an original Clojure implementation. All of the code is an interpretation
+   of the general concepts described in the textbook. This code is the original
+   work of the author (bitfl1pper). Obviously, the code is merely a derivative of the
+   logic described in the textbook.
+
+   The maths of the algorithm can be found on pages # #2 #3.
+
+   I plan on licensing this code to be as free as possible. But I don't really
+   know much about licensing, so that's on my todo list.
+
+   For now, assume Eclispse Public License. This code must retain citations.
+   Most importantly: Needleman / Wunsch et al., the Handbook [...], and this
+   author.
+
    [*, **] Citations in resource directory.")
 
 
